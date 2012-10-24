91ba86caffe0a8f09a31d92a4016485ac24ea13d
lijunsong <ljs.darkfish@gmail.com>
2012-10-24 12:03:26 +0800

    add many claases

--- auto.py
+++ auto.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 class Course:
     def __init__(self, name, credit, grade, teacher, time=None):
         self.name = name
@@ -8,14 +7,38 @@ class Course:
         self.grade = grade
         self.teacher = teacher
         self.time = time
+    def __str__(self):
+        return "name:%s credit:%s grade:%s tcher:%s time:%s" \
+                % (self.name, self.credit, self.grade, self.teacher, self.time)
 
+class CourseTable:
+    def __init__(self):
+        self.period = ["08:00", "10:10", "13:00", "16:10"]
+        self.coursetable = [[-1 for col in range(5)] for row in period]
+    def set_course(course, posx, posy):
+        self.coursetable[posx][posy] = course
+    def unset_course(course,posx, posy):
+        self.coursetable[posx][posy] = -1
+    
 class Grade:
-    def __init__(self, name, course):
+    def __init__(self, name, courses):
         self.name = name
-        self.course = course
-        self.course_table = None
+        self.courses = courses
+        self.course_table = CourseTable()
 
-class Reader:
+    def __str__(self):
+        s = "=======Grade: %s=======\n" % self.name
+        for c in self.courses:
+            s += str(c) + "\n"
+        return s
+
+    def set_course(self, course, posx, posy):
+        self.course_table.set_course(course, posx, posy)
+
+    def unset_course(self, course, posx, posy):
+        self.course_table.unset_course(course, posx, posy)
+
+class ReadCourse:
     def __init__(self, file_name, delimiter=","):
         self.file_name = file_name
         self.delimiter = delimiter
@@ -40,22 +63,72 @@ class Reader:
     def __process_csv(self):
         contents = open(self.file_name).readlines()
         for content in contents:
+            if content.startswith("#"):
+                continue
             course = self.__course_obj(content)
             self.courses.append(course)
             
-def pretty_courses():
-    for c in courses:
-        print c.__dict__
+class CourseFilter:
+    def __init__(self, courses):
+        self.courses = courses
 
+    def grade_courses(self, grade):
+        result = []
+        for c in self.courses:
+            if c.grade == grade:
+                result.append(c)
+        return result
+
+    def get_total_grades(self):
+        result = []
+        for c in self.courses:
+            try:
+                result.index(c.grade)
+            except ValueError:
+                result.append(c.grade)
+        return result
+
+    def get_grade_objs(self):
+        result = []
+        gs = self.get_total_grades()
+        for g in gs:
+            name = g
+            cs = self.grade_courses(name)
+            result.append(Grade(name, cs))
+        return result
+
+def printt(os):
+    if type(os) == types.ListType:
+        for o in os:
+            print o
+    else:
+        print os
+
+class Generator:
+    def __init__(self, grades):
+        self.grades = grades
+
+    # Grade * int * int * int * int => boolean
+    def generate(self, which_grade, which_course, total_course, which_posx, which_posy): #return boolean
+        which_grade.course_table.set_course(which_course, which_posx, which_posy)
+        if which_course == total_course - 1: # successfully put all courses
+            return True
+        for next_posx in range(which_posx+1, 5):
+            for next_posy in range(which_posy+1, 4):
+                self.generate(which_grade, which_course + 1, total_course, next_posx, next_posy)
+        which_grade.course_table.unset_course(which_course, which_posx, which_posy)
+        
+        
 if __name__ == '__main__':
     import sys
+    import types
 
-    period = ["8:00", "9:00", "10:10", "11:10", "12:10", "14:00", \
-              "15:00", "16:10", "17:10"]
-    timetable = [[col for col in range(7)] for row in period]
-    
     assert len(sys.argv) == 2
 
-    reader = Reader(sys.argv[1])
+    reader = ReadCourse(sys.argv[1])
     courses = reader.get_courses()
+    flter = CourseFilter(courses)
+    grade = flter.get_total_grades()
 
+    grades = flter.get_grade_objs()
+    printt(grades)
